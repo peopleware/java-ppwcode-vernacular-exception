d692c859d52a8cd0abf09355d1a2035b2347faf2
Jan Dockx <jan_dockx@peopleware.be>
2004-12-01 10:26:08 +0000

    Refactoring work of yesterday. Added and adapted some comments.
    git-svn-id: file:///repositories/PPWCode/trunk@94 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- contractTaglets/src/java/be/peopleware/taglet/AbstractTaglet.java
+++ contractTaglets/src/java/be/peopleware/taglet/AbstractTaglet.java
@@ -10,7 +10,10 @@ import com.sun.tools.doclets.Taglet;
  * <br /> Custom taglets can extend this class and define scopes 
  * where they are valid.</p>
  *
- * @invar     getName() != null && getName().length() > 0
+ * @invar     getName() != null && getName().length() > 0;
+ * 
+ * @idea tags below are not real, but are here to demonstrate the
+ *       taglets in this package themselves.
  *
  * @mudo (UnitTest): demo must do: test 
  *
@@ -20,76 +23,83 @@ import com.sun.tools.doclets.Taglet;
  *
  * @author    Jan Dockx
  * @author    David Van Keer
- * @author    René Clerckx
+ * @author    Ren&eacute; Clerckx
  * @author    Abdulvakhid Shoudouev
  * @author    Peopleware n.v.
+ * 
+ *
+ * @invar getName() != null;
+ * @invar getName().getLength() > 0;
  */
 public abstract class AbstractTaglet implements Taglet {
   
   /**
    * Used to determine if this taglet can be used in 
    * <strong>field documentation</strong>.
-   * <br />Default value is false.
+   * <br />Default value is <code>false</code>.
    */
   protected boolean $inField = false;
 
   /**
    * Used to determine if this taglet can be used in 
-   * <strong>constructor documentation </strong>.
-   * <br />Default value is false.
+   * <strong>constructor documentation</strong>.
+   * <br />Default value is <code>false</code>.
    */
   protected boolean $inConstructor = false;
 
   /**
    * Used to determine if this taglet can be used in 
-   * <strong>method documentation </strong>.
-   * <br />Default value is false.
+   * <strong>method documentation</strong>.
+   * <br />Default value is <code>false</code>.
    */
   protected boolean $inMethod = false;
 
   /**
    * Used to determine if this taglet can be used in 
-   * <strong>overview documentation </strong>.
-   * <br />Default value is false.
+   * <strong>overview documentation</strong>.
+   * <br />Default value is <code>false</code>.
    */
   protected boolean $inOverview = false;
 
   /**
    * Used to determine if this taglet can be used in 
-   * <strong>package documentation </strong>.
-   * <br />Default value is false.
+   * <strong>package documentation</strong>.
+   * <br />Default value is <code>false</code>.
    */
   protected boolean $inPackage = false;
 
   /**
    * Used to determine if this taglet can be used in 
-   * <strong>type documentation </strong>.
-   * <br />Default value is false.
+   * <strong>type documentation</strong>.
+   * <br />Default value is <code>false</code>.
    */
   protected boolean $inType = false;
 
   /**
    * Used to determine if this taglet is an 
-   * <strong>inline taglet </strong>.
-   * <br />Default value is false.
+   * <strong>inline taglet</strong>.
+   * <br />Default value is <code>false</code>.
    */
   protected boolean $inLine = false;
 
   /**
-   * Use constructor in your subclasses to define scopes 
+   * <p>Use a (default) constructor in your subclasses to define scopes 
    * where this taglet can be used. Setting certain boolean
    * variable to true enables usage in corresponding documentation scope. 
-   * For example, taglet <code>todo</code> would have following settings:
-   * <br /><code>
-   * <br />$inField       = true;
-   * <br />$inConstructor = true;
-   * <br />$inMethod       = true;
-   * <br />$inOverview     = true;
-   * <br />$inPackage    = true;
-   * <br />$inType         = true;
-   * </code><br /><br />
-   *
-   * By default, all variables are set to false.
+   * E.g.:</p>
+   * <pre>
+   *   $inField       = true;
+   *   $inConstructor = true;
+   *   $inMethod      = true;
+   *   $inOverview    = true;
+   *   $inPackage     = true;
+   *   $inType        = true;
+   * </pre>
+   * <p>By default, all variables are set to <code>false</code>.</p>
+   * 
+   * @post new.inField() == false;
+   * 
+   * @mudo (Jan -> Abdul) ALLEMAAL NAAR VOORBEELD INFIELD
    */
   protected AbstractTaglet() {
     // NOP
@@ -100,25 +110,27 @@ public abstract class AbstractTaglet implements Taglet {
    *
    * @param     text
    *            content of the taglet
-   * @return    text - formatted content
+   * @pre       text != null;
    */
   public abstract String parse(String text);
 
   /**
    * Return the name of this custom taglet.
-   *
-   * @result    getName() != null && getName().getLength() > 0
+   * 
+   * @basic
    */
   public abstract String getName();
 
   /**
-   * Used to determine if this taglet can be used in 
-   * <strong>field documentation </strong>. 
-   * If you wish to override default behavior, which always returns false, 
-   * set the value of variable {@link #$inField} to true 
-   * in the constructor of your class. 
-   *
-   * @return    value of {@link #$inField}
+   * <p>Used to determine if this taglet can be used in 
+   * <strong>field documentation </strong>.</p> 
+   * 
+   * @protected
+   * <p>If you wish to override default behavior, which always
+   *  returns <code>false</code>, set the value of variable
+   *  {@link #$inField} to true in the constructor of your class.</p>
+   * 
+   * @basic
    */
   public final boolean inField() {
     return $inField;
@@ -127,7 +139,7 @@ public abstract class AbstractTaglet implements Taglet {
   /**
    * Used to determine if this taglet can be used in 
    * <strong>constructor documentation </strong>. 
-   * If you wish to override default behavior, which always returns false, 
+   * If you wish to override default behavior, which always returns <code>false</code>, 
    * set the value of variable {@link #$inConstructor} to true 
    * in the constructor of your class.
    *
@@ -140,7 +152,7 @@ public abstract class AbstractTaglet implements Taglet {
   /**
    * Used to determine if this taglet can be used in 
    * <strong>method documentation </strong>. 
-   * If you wish to override default behavior, which always returns false, 
+   * If you wish to override default behavior, which always returns <code>false</code>, 
    * set the value of variable {@link #$inMethod} to true
    * in the constructor of your class.
    *
@@ -153,7 +165,7 @@ public abstract class AbstractTaglet implements Taglet {
   /**
    * Used to determine if this taglet can be used in 
    * <strong>overview documentation </strong>. 
-   * If you wish to override default behavior, which always returns false, 
+   * If you wish to override default behavior, which always returns <code>false</code>, 
    * set the value of variable {@link #$inOverview} to true 
    * in the constructor of your class.
    *
@@ -166,7 +178,7 @@ public abstract class AbstractTaglet implements Taglet {
   /**
    * Used to determine if this taglet can be used in 
    * <strong>package documentation </strong>. 
-   * If you wish to override default behavior, which always returns false, 
+   * If you wish to override default behavior, which always returns <code>false</code>, 
    * set the value of variable {@link #$inPackage} to true
    * in the constructor of your class.
    *
@@ -179,7 +191,7 @@ public abstract class AbstractTaglet implements Taglet {
   /**
    * Used to determine if this taglet can be used in 
    * <strong>type documentation </strong>. 
-   * If you wish to override default behavior, which always returns false, 
+   * If you wish to override default behavior, which always returns <code>false</code>, 
    * set the value of variable {@link #$inType} to true 
    * in the constructor of your class.
    *
@@ -196,27 +208,35 @@ public abstract class AbstractTaglet implements Taglet {
   public abstract boolean isInlineTag();
 
   /**
-   * Given the <code>com.sun.javadoc.Tag</code> representation of this custom
+   * Given the {@link Tag} representation of this custom
    * taglet, return its string representation.
+   * This method is called by the standard doclet if this is the
+   * last tag of this type in the documentation block, or if
+   * the next tag is of a different type.
+   * 
+   * @see #toString(Tag[])
    *
    * @param     taglet
    *            the <code>com.sun.javadoc.Tag</code> representation of this
    *            custom taglet.
-   *
-   * @result    (taglet != null) ? result == "valid html formatting code"
-   *                             : result == null
+   * @pre taglet != null;
    */
   public abstract String toString(Tag taglet);
 
   /**
-   * Given an array of <code>com.sun.javadoc.Tag</code> s representing this
+   * Given an array of {@link Tag}s representing this
    * custom taglet, return its string representation.
+   * This method is called by the standard doclet if there
+   * are more then 1 consecutive tags of the same type.
+   * 
+   * @see #toString(Tag)
    *
    * @param     taglets
    *            the array of <code>com.sun.javadoc.Tag</code> s representing of
    *            this custom taglet.
-   * @result    (taglets != null) ? result == "valid html formatting code"
-   *                              : result == null
+   * @pre taglets != null
+   * @pre (forall int i; (i >= 0) && (i < taglets.length);
+   *            taglets[i] != null);
    */
   public abstract String toString(Tag[] taglets);
 
