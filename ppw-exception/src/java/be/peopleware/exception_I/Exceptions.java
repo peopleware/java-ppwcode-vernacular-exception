44cb4dd157c11149b1ca6f708225a7aa5b2165aa
Jan Dockx <jan_dockx@peopleware.be>
2004-12-22 15:28:51 +0000

    First import of exceptions library
    git-svn-id: file:///repositories/PPWCode/trunk@125 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

---
+++ ppw-exception/src/java/be/peopleware/exception_I/Exceptions.java
@@ -0,0 +1,77 @@
+package be.peopleware.exception_I;
+
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.el.ELException;
+
+
+/**
+ * Convenience methods for working with {@link java.lang.Exception}s.
+ *
+ * @author      Jan Dockx
+ * @author      PeopleWare n.v.
+ */
+public class Exceptions {
+
+  /*<section name="Meta Information">*/
+  //------------------------------------------------------------------
+
+  /** {@value} */
+  public static final String CVS_REVISION = "$Revision$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_DATE = "$Date$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_STATE = "$State$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_TAG = "$Name$"; //$NON-NLS-1$
+
+  /*</section>*/
+
+
+
+  /**
+   * Look in the {@link Throwable#getCause() cause},
+   * {@link JspException#getRootCause() "root cause"} or
+   * {@link ELException#getRootCause() "root cause"} for an exception
+   * of type <code>exceptionType</code>. <code>null</code> is returned
+   * if no such cause is found.
+   *
+   * @param     exc
+   *            The exception to look in.
+   * @param     exceptionType
+   *            The type of Exception to look for
+   *
+   * @pre       exceptionType != null;
+   * @result    ((exc == null) || (exceptionType.isInstance(exc)))
+   *                ==> (result == exc);
+   * @result    ((exc != null) && (! exceptionType.isInstance(exc)))
+   *                ==> ((result == huntFor(exc.getRootCause)
+   *                    || (result == huntFor(exc.getCause)))
+   * @result    (result != null) ==> exceptionType.isInstance(result);
+   */
+  public static Throwable huntFor(final Throwable exc,
+                                  final Class exceptionType) {
+    Throwable result = null;
+    if ((exc != null) && (exceptionType.isInstance(exc))) {
+      result = exc;
+    }
+    else if (exc != null) {
+      if (exc instanceof JspException) {
+        result = huntFor(((JspException)exc).getRootCause(),
+                         // if ClassCastException, we fail grand
+                         exceptionType);
+      }
+      else if (exc instanceof ELException) {
+        result = huntFor(((ELException)exc).getRootCause(),
+                         // if ClassCastException, we fail grand
+                         exceptionType);
+      }
+      if (result == null) {
+        result = huntFor(exc.getCause(), exceptionType);
+                         // if ClassCastException, we fail grand
+      }
+    }
+    return result;
+  }
+
+}
