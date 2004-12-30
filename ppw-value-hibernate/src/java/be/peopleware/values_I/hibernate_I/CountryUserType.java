6f4fe661fae2ae5948cb6945906a6abc969de87f
Jan Dockx <jan_dockx@peopleware.be>
2004-12-30 10:35:37 +0000

    Import
    git-svn-id: file:///repositories/PPWCode/trunk@154 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

---
+++ ppw-value-hibernate/src/java/be/peopleware/values_I/hibernate_I/CountryUserType.java
@@ -0,0 +1,37 @@
+package be.peopleware.values_I.hibernate_I;
+
+
+import be.peopleware.value_I.Country;
+import be.peopleware.value_I.CountryEditor;
+
+
+/**
+ * Hibernate mapping for {@link Country}. Country ISO codes
+ * are stored in a VARCHAR.
+ *
+ * @author    Jan Dockx
+ * @author    Peopleware n.v.
+ */
+
+public final class CountryUserType extends AbstractEnumerationUserType {
+
+  /*<section name="Meta Information">*/
+  //  ------------------------------------------------------------------
+
+  /** {@value} */
+  public static final String CVS_REVISION = "$Revision$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_DATE = "$Date$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_STATE = "$State$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_TAG = "$Name$"; //$NON-NLS-1$
+
+  /*</section>*/
+
+
+  public CountryUserType() {
+    super(new CountryEditor());
+  }
+
+}
