148e3452470757db558ef9dde93632532ba94e71
David Van Keer <david_van_keer@peopleware.be>
2005-08-09 14:09:34 +0000

    Added type to PersistentBeanConvertor
    git-svn-id: file:///repositories/PPWCode/trunk@510 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- ppw-jsf/src/java/be/peopleware/jsf_II/convert/PersistentBeanConverter.java
+++ ppw-jsf/src/java/be/peopleware/jsf_II/convert/PersistentBeanConverter.java
@@ -5,12 +5,14 @@
 package be.peopleware.jsf_II.convert;
 
 
+import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
-
 import net.sf.hibernate.Session;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import be.peopleware.exception_I.TechnicalException;
 import be.peopleware.jsf_II.RobustCurrent;
 import be.peopleware.persistence_I.DaoFactory;
@@ -30,7 +32,7 @@ import be.peopleware.servlet_I.hibernate.SessionInView;
  * 
  *  &lt;converter&gt;
  *    &lt;converter-for-class&gt;be.peopleware.persistence_I.PersistentBean&lt;/converter-for-class&gt;
- *    &lt;converter-class&gt;be.peopleware.jsf_II.convertor.ResponsibilyDelegatorConverter&lt;/converter-class&gt;
+ *    &lt;converter-class&gt;be.peopleware.jsf_II.convertor.PersistentBeanConverter&lt;/converter-class&gt;
  *  &lt;/converter&gt;
  *  
  * </pre>
@@ -51,34 +53,49 @@ public class PersistentBeanConverter implements Converter {
   /** {@value} */
   public static final String CVS_TAG = "$Name$"; //$NON-NLS-1$
   /* </section> */
+  
+  private static final Log LOG = LogFactory.getLog(PersistentBeanConverter.class);
+  
 
   /**
+   * @param     context
+   *            The facesContext to work with
+   * @param     component
+   *            TODO
+   * @param     value
+   *            TODO
    * @return    (value != null)
-   *              ? TODO...
+   *              ? result.getId().equals((Long)value))
    *              : null;
    * @throws    ConverterException
-   *            TODO
+   *            No PersistentBean was found with a ID of {@param value} or
+   *            technical problems occured while trying to retrieve the correct
+   *            PersistentBean
    */
   public Object getAsObject(final FacesContext context,
                             final UIComponent component,
                             final String value)
       throws ConverterException {
     Object result = null;
+    // FatalFacesException if Type is null...
     if (value != null && !value.equals(EMPTY)) {
       Long id = Long.valueOf(value);
-      // Retrieve PersistentBean based on the ID found and return it.
+      LOG.debug("Recieving a request for a \"" + getType() + "\" with id \"" + id + "\".");
       HibernateDaoFactory daoFactory = new HibernateDaoFactory();
       HibernateAsyncCrudDao dao = (HibernateAsyncCrudDao)daoFactory.getDao(DaoFactory.ASYNC_CRUD);
       try {
         Session session = SessionInView.getSession(RobustCurrent.httpServletRequest());
         dao.setSession(session);
-        result = dao.retrievePersistentBean(id, PersistentBean.class);
+        result = dao.retrievePersistentBean(id, getType());
+        LOG.debug("\"" + getType() + "\" retrieved with id \"" + id + "\"");
       }
       catch (IdNotFoundException infExc) {
+        LOG.error("No \"" + getType() + "\" found with id \"" + id + "\"");
         throw new ConverterException("No PersistentBean bean found with id " + id);
       }
       catch (TechnicalException tExc) {
-        throw new ConverterException("Unable to retrieve PersistentBean.");
+        LOG.error("Failed to retrieve \"" + getType() + "\" due to technical problems", tExc);
+        throw new ConverterException("Unable to retrieve PersistentBean due to technical problems.");
       }
     }
     return result;
@@ -87,6 +104,12 @@ public class PersistentBeanConverter implements Converter {
   public final static String EMPTY = "";
 
   /**
+   * @param     context
+   *            The facesContext to work with
+   * @param     component
+   *            TODO
+   * @param     value
+   *            TODO
    * @return    (value != null)
    *                ? (((PersistentBean)value).getId() != null)
    *                      ? ((PersistentBean)value).getId().toString
@@ -102,9 +125,68 @@ public class PersistentBeanConverter implements Converter {
       return EMPTY;
     }
     if (!(value instanceof PersistentBean)) {
+      LOG.error("Unable to convert \"" + value + "\" because it is not a subclass of PersistentBean.");
       throw new ConverterException("Value is not subclass of PersistentBean Class");
     }
     PersistentBean persistentBean = ((PersistentBean)value);
     return (persistentBean.getId() != null) ? persistentBean.getId().toString() : EMPTY;
   }
+  
+  /*<property name="type">*/
+  //------------------------------------------------------------------
+
+  /**
+   * The type of the object (subclass of PersistentBean) that will
+   * be used as retrieval type.
+   *
+   * @basic
+   * @init null;
+   */
+  public final Class getType() {
+    return $type;
+  }
+  
+  public final String getTypeName() {
+    return $type.getName();
+  }
+
+  /**
+   * Set the type of the object (subclass of PersistentBean) that will
+   * be used as retrieval type.
+   *
+   * @param   typeName
+   *          The fully qualified name of the type to be set.
+   * @pre     (Class.forName(typeName) != null)
+   *              ? PersistentBean.class.isAssignableFrom(Class.forName(typeName));
+   * @post    getType() == Class.forName(type);
+   * @throws  FacesException
+   *          {@link Class#forName(String)}
+   */
+  public void setTypeName(final String typeName) throws FacesException {
+    Class type;
+    try {
+      type = Class.forName(typeName);
+    }
+    catch (LinkageError lErr) {
+      throw new FacesException("cannot convert String to Class", lErr);
+    }
+    catch (ClassNotFoundException cnfExc) {
+      throw new FacesException("cannot convert String to Class", cnfExc);
+    }
+    assert ((type != null)
+        ? PersistentBean.class.isAssignableFrom(type) : true)
+        : "Type must be a subtype of " + PersistentBean.class +
+            " (and " + type + " isn't).";
+    $type = type;
+    LOG.debug("type of " + this + " set to Class " + type.getName());
+  }
+
+  /**
+   * The type of the {@link PersistentBean} that will be handled
+   * by the requests.
+   */
+  private Class $type;
+
+  /*</property>*/
+  
 }
