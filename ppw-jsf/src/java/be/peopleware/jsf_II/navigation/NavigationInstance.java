4489fbc0a327f44072ce841febb2785d152b7bb7
Jan Dockx <jan_dockx@peopleware.be>
2005-08-17 22:33:13 +0000

    Working on back navigation stack. This compiles, doc is here, but not finished.
    git-svn-id: file:///repositories/PPWCode/trunk@601 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

---
+++ ppw-jsf/src/java/be/peopleware/jsf_II/navigation/NavigationInstance.java
@@ -0,0 +1,127 @@
+/*<license>
+  Copyright 2004-2005, PeopleWare n.v.
+  NO RIGHTS ARE GRANTED FOR THE USE OF THIS SOFTWARE, EXCEPT, IN WRITING,
+  TO SELECTED PARTIES.
+</license>*/
+
+
+package be.peopleware.jsf_II.navigation;
+
+
+import java.util.Date;
+
+
+/**
+ * <p>Keep the information about a step in the navigation through a
+ *   web application. Instances of this type are used in a
+ *   {@link NavigationStack}.</p>
+ * <p>This interface does not concern itself with the exact definition
+ *   of a navigation instance, or how to return to a previous state, or what to
+ *   do in case of problems. This is a thin interface, and
+ *   different implementations can be used for different strategies. This package
+ *   offers a number of concrete implementations, for use cases that are
+ *   supported elsewhere in this library, but application developers can develop
+ *   more.</p>
+ * <p>Possbible implementations are not limited to JSF navigation, although
+ *   the concrete implementations in this package do depend on JSF.</p>
+ * <p>Absorption is inspired by {@link javax.swing.undo.UndoableEdit}.</p>
+ *
+ * @author Jan Dockx
+ * @author PeopleWare n.v.
+ */
+public interface NavigationInstance {
+
+  /*<section name="Meta Information">*/
+  //------------------------------------------------------------------
+  /** {@value} */
+  public static final String CVS_REVISION = "$Revision$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_DATE = "$Date$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_STATE = "$State$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_TAG = "$Name$"; //$NON-NLS-1$
+  /*</section>*/
+
+
+  /*<property name="time">*/
+  //------------------------------------------------------------------
+
+  /**
+   * @basic
+   * @init NOW;
+   */
+  Date getTime();
+
+  /*</property>*/
+
+
+  /**
+   * Join the state of this <code>NavigationInstance</code> and <code>ni</code>
+   * into <code>result</code>. This is typically called on an instance
+   * on top of the {@link NavigationStack}, with a next <code>NavigationInstance</code>.
+   * If this succeeds, the current top of the stack is replaced with the result
+   * of this operation, and <code>ni</code> is not placed on the stack. This
+   * method may return <code>this</code>, possibly
+   * with changed state to take into account the <code>ni</code> navigation.
+   * If absorption is not possible, return <code>null</code>. When <code>ni</code> is
+   * <code>null</code>, return <code>null</code>.
+   *
+   * @result (ni == null) ? (result == null);
+   */
+  NavigationInstance absorb(NavigationInstance ni);
+
+  /**
+   * Navigate back to this navigation instance.
+   */
+  void goBack();
+
+
+//  /**
+//   * <p>Set the {@link InstanceHandler#setId(Long) id} of
+//   * {@link #getFromHandler()} to {@link #getFromPersistentBeanId()}, tell it to
+//   * {@link InstanceHandler#reset() reset}, and <strong>HTTP redirect</strong> to
+//   * {@link #getFromLink()}.</p>
+//   *
+//   * <p>This should should result in the browser requesting the
+//   * {@link #getFromViewId() from JSF page} from the web app. This page should use
+//   * {@link #getFromHandler()} as its handler, which was just prepared
+//   * for such a request.</p>
+//   *
+//   * <p>This method should be used as an <dfn>action method</dfn> like</p>
+//   * <pre>
+//   * &lt;h:commandButton id=&quot;<var>backButtonId</var>&quot;
+//   *        value=&quot;#{<var>buttonBundle</var>.<var>backButtonLabel</var>}&quot;
+//   *        action=&quot;#{<var>navigationStack</var>.pop.go}&quot;
+//   *        immediate=&quot;true&quot;
+//   *        disabled=&quot;#{not <var>currentHandler</var>.displayView}&quot; /&gt;
+//   * </pre>
+//   *
+//   * @pre getOutcomeViewId.equals(CUrrentVIewID); <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//   * KAN WEL; ZELFDE PAGINA VOOR ANDERE ID
+//   * @post getFromHandler().getPersistentBeanId().equals(getFromPersistentBeanId());
+//   * @post getFromHandler().reset();
+//   * @throws FacesException
+//   *         RobustCurrent.facesContext();
+//   * @throws FacesException
+//   *         RobustCurrent.redirect();
+//   */
+//  public final void go() throws FacesException {
+//    if (LOG.isDebugEnabled()) {
+//      String currentViewId = RobustCurrent.uiViewRoot().getViewId();
+//      LOG.debug("go called from " + currentViewId);
+//    }
+//    getFromHandler().setId(getFromPersistentBeanId());
+//    // TODO getFromPersistentBeanId() cannot be null
+//    LOG.debug("handler id set and handler reset: " + getFromHandler()
+//              + " (id: " + getFromPersistentBeanId() + ")");
+//    String fromLink = getFromLink();
+//    RobustCurrent.redirect(fromLink);
+//    RobustCurrent.facesContext().responseComplete();
+//    LOG.debug("redirected to from link: " + fromLink);
+//  }
+
+}
+
+
+
---
+++ ppw-jsf/src/java/be/peopleware/jsf_II/navigation/NavigationStack.java
@@ -0,0 +1,249 @@
+/*<license>
+  Copyright 2004-2005, PeopleWare n.v.
+  NO RIGHTS ARE GRANTED FOR THE USE OF THIS SOFTWARE, EXCEPT, IN WRITING,
+  TO SELECTED PARTIES.
+</license>*/
+
+
+package be.peopleware.jsf_II.navigation;
+
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+import javax.faces.FacesException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import be.peopleware.jsf_II.FatalFacesException;
+import be.peopleware.jsf_II.RobustCurrent;
+import be.peopleware.jsf_II.persistence.PersistentBeanHandler;
+
+
+/**
+ * <p>Stack of user navigation in a user session. The most important goal of this
+ *   class is to provide goBack-functionality. An instance of this type should
+ *   be declared as a managed bean in session scope:</p>
+ * <pre>
+ *   &lt;managed-bean&gt;
+ *     &lt;managed-bean-name&gt;navigationStack&lt;/managed-bean-name&gt;
+ *     &lt;managed-bean-class&gt;be.peopleware.jsf_II.navigation.NavigationStack&lt;/managed-bean-class&gt;
+ *     &lt;managed-bean-scope&gt;session&lt;/managed-bean-scope&gt;
+ *   &lt;/managed-bean&gt;
+ * </pre>
+ * <p>Suppose the user navigate from a <dfn>page A</dfn> to a <dfn>page B</dfn>.
+ *   At this time, the top of the stack will be a {@ NavigationInstance} that
+ *   holds all necssary information to bring the user back to his current state.
+ *   The second entry on the stack will be a {@link NavigationInstance} that
+ *   holds all necessary information to bring the user back to the state
+ *   when his browser showed <dfn>page A</dfn>.</p>
+ * <p>The term <dfn>page</dfn> above should be taken losely. E.g., in an
+ *   application that shows semantic data in different modes (display, edit,
+ *   deleted, ?, see {@link PersistentBeanHandler}), the architect might decide
+ *   that all operations on 1 whole of semantic data are considered 1
+ *   {@link NavigationInstance}. This would mean that returning to the recorded
+ *   state results in showing the same semantic data, in a default mode
+ *   (e.g., display). The mode state is forgotten. Also, if e.g., a persistent
+ *   object was shown in a previous state, and it is deleted from persistent
+ *   storage in the mean time, that returning to a previous state is not possible.
+ *   The application could then retrace the user on step further, e.g., and
+ *   display a message to this effect.</p>
+ * <p>Instances of this type do not consider themselves with the exact definition
+ *   of a navigation instance, and how to return to a previous state, or what to
+ *   do in case of problems. {@link NavigationInstance} is a thin interface, and
+ *   different implementations can be used for different strategies. This package
+ *   offers a number of concrete implementations, for use cases that are
+ *   supported elsewhere in this library, but application developers can develop
+ *   more.</p>
+ *
+ * <h3>Recording Navigation</h3>
+ * <p>The main problem that poses itself is how to record navigation state. For this,
+ *   we have to consider the possible ways of navigation, and the different resources
+ *   that can be requested from a server.</p>
+ * <p>First, there is regular JSF navigation. This means, when navigating from
+ *   page A to page B, that an action method or action listener method in a
+ *   handler for page A is called, which returns an outcome that resolves to page
+ *   B, or navigates to page B itself.</p>
+ * <p>Second, there is navigation by an uncontrolled HTTP request. This happens, e.g.,
+ *   for images in a page, downloads, or when the user just types in a URL in his
+ *   browser, when he activates a bookmark, or when the JSF developer used, e.g., an
+ *   <code>outputLink</code>. In this case, most often, images, downloads, or other
+ *   such resources, are not considered navigation, and thus no action should be
+ *   taken. Uncontrolled navigation to a page is relevant. When uncontrolled navigation
+ *   to a page B happens, we are essentially in the same situation as above, where
+ *   we at JSF navigation from a page A to page B.</p>
+ * <p>To exploit this commonality, navigation recording should be independent of
+ *   how we get to a JSF page. In other words, it should happen during or after
+ *   the <dfn>render response</dfn> phase. This is also necessary in the first case,
+ *   because, to be able to render a JSF page again in a previous state, also the
+ *   handlers need to be restored to their previous state. Only the target page B
+ *   author can know, in general, which handlers are used, and how to restore them,
+ *   and before the <dfn>render response</dfn>, are not in the context of the target
+ *   page B. Only in very special circumstances would it be possible to record state
+ *   earlier, but even then here we need be cautious: it is possible that an error
+ *   occurs during rendering of the response, and in that case the stack would
+ *   represent a wrong state. On the other hands, if an error occurs during the
+ *   render response phase, you probably would end the user session anyway.
+ *   A phase listener could possibly be used, but in general a custom component
+ *   in the page seems the best way to record navigation.</p>
+ * <p>It is also possible that there are regular JSP pages that are part of your
+ *   web application, or even plain HTML pages. For regular JSP pages, also a
+ *   custom tag, or a scriptlet, could be used to record navigation. For regular
+ *   HTML pages, we would need to use, e.g., a {@link javax.servlet.Filter}.</p>
+ * <p>In the subpackage {@link be.pepleware.jsf_II.navigation.persistence}, we
+ *   offer support for the default navigation offered in
+ *   {@link be.peopleware.jsf_II.persistence}.</p>
+ *
+ * @idea This opens possibilities for detecting that a user makes cycles, or
+ *   has returned to a page much earlier.
+ *
+ * @author Jan Dockx
+ * @author PeopleWare n.v.
+ *
+ * @invar getSize() >= 0;
+ * @invar isEmpty() ? getTop() == null;
+ */
+public class NavigationStack {
+
+  /*<section name="Meta Information">*/
+  //------------------------------------------------------------------
+  /** {@value} */
+  public static final String CVS_REVISION = "$Revision$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_DATE = "$Date$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_STATE = "$State$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_TAG = "$Name$"; //$NON-NLS-1$
+  /*</section>*/
+
+
+  private static final Log LOG = LogFactory.getLog(NavigationStack.class);
+
+
+  /**
+   * The {@link NavigationInstance} describing the current page the user
+   * is seeing in his browser.
+   *
+   * @basic
+   */
+  public final NavigationInstance getTop() {
+    try {
+      return (NavigationInstance)$stack.getLast();
+    }
+    catch (NoSuchElementException nseExc) {
+      return null;
+    }
+  }
+
+  /**
+   * @basic
+   * @init 0;
+   */
+  public final int getSize() {
+    return $stack.size();
+  }
+
+  /**
+   * @return getSize() == 0;
+   */
+  public final boolean isEmpty() {
+    return $stack.isEmpty();
+  }
+
+
+  /**
+   * Add a {@link NavigationInstance} to the top of the stack.
+   * First, we ask the old top of the stack to
+   * {@link NavigationInstance#absorb(NavigationInstance) absorb}
+   * the new {@link NavigationInstance} <code>ni</code>. If this succeeds,
+   * and {@link NavigationInstance#absorb(NavigationInstance)} has a result
+   * that is not <code>null</code>, the current top is replaced by that
+   * result. If this does return <code>null</code>, <code>ni</code>
+   * is added to the stack and becomes the new {@link #getTop()}.
+   * Pushing <code>null</code> results in an exception.
+   *
+   * @post (isEmpty() || (getTop().absorb(ni) == null)) ?
+   *            new.getSize() == getSize() + 1 :
+   *            new.getSize() == getSize();
+   * @post (isEmpty() || (getTop().absorb(ni) == null)) ?
+   *            new.getTop() == ni :
+   *            new.getTop() == getTop().absorb(ni);
+   * @throws FatalFacesException
+   *         ni == null;
+   */
+  public final void push(NavigationInstance ni) throws FatalFacesException {
+    LOG.debug("pushing on stack: " + ni);
+    if (ni == null) {
+      RobustCurrent.fatalProblem("tried to add null to navigation stack", LOG);
+    }
+    NavigationInstance toPush = ni;
+    if (! isEmpty()) {
+      NavigationInstance joined = getTop().absorb(ni); // NullPointerException cannot happen
+      if (joined != null) {
+        pop(); // exception cannot happen
+        toPush = joined;
+      }
+    }
+    $stack.add(toPush);
+  }
+
+  /**
+   * Pop the top of the stack.
+   *
+   * @post new.getSize() == getSize() - 1;
+   * @throws FatalFacesException
+   *         isEmpty();
+   */
+  public final void pop() throws FatalFacesException {
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("popping navigation stack (top is " + getTop() + ")");
+    }
+    try {
+      $stack.removeLast();
+    }
+    catch (NoSuchElementException nseExc) {
+      RobustCurrent.fatalProblem("stack was empty; cannot pop", LOG);
+    }
+  }
+
+  /**
+   * @toryt-cC org.toryt.contract.Collections;
+   * @invar $stack != null;
+   * @invar cC:noNull($stack);
+   * @invar cC:instanceOf($stack, NavigationInstance.class);
+   */
+  private final LinkedList $stack = new LinkedList();
+
+  /**
+   * Navigate back to the previous user state. The old {@link #getTop()}
+   * is the page the user is viewing currently. The {@link NavigationInstance}
+   * immediately below the top describes the state the user wants to return
+   * to. This will become the new current state. However, by re-rendering
+   * this state, a new entry for this state will be pushed to this stack.
+   * This means that we need to remove the 2 entries at the top, and navigate
+   * to the second one.
+   *
+   * @post new.getSize() = getSize() - 2;
+   * @post pop(); getTop().goBack();
+   * @throws FacesException
+   *         getSize() < 2;
+   * @throws FacesException
+   *         pop(); getTop().goBack();
+   */
+  public final void goBack() throws FacesException {
+    if (getSize() < 2) {
+      RobustCurrent.fatalProblem("cannot go back: no previous state in NavigationStack", LOG);
+    }
+    pop();
+    NavigationInstance previous = getTop();
+    pop();
+    LOG.debug("navigating back to previous state (" + previous + ")");
+    previous.goBack();
+  }
+
+}
+
+
+
