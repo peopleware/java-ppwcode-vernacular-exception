e6cbbd6b2aac3653bfd13e3aa30ad664f359260c
Jan Dockx <jan_dockx@peopleware.be>
2005-08-17 22:44:02 +0000

    Common code for NavigationInstances.
    git-svn-id: file:///repositories/PPWCode/trunk@602 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

---
+++ ppw-jsf/src/java/be/peopleware/jsf_II/navigation/AbstractNavigationInstance.java
@@ -0,0 +1,60 @@
+/*<license>
+  Copyright 2004-2005, PeopleWare n.v.
+  NO RIGHTS ARE GRANTED FOR THE USE OF THIS SOFTWARE, EXCEPT, IN WRITING,
+  TO SELECTED PARTIES.
+</license>*/
+
+
+package be.peopleware.jsf_II.navigation;
+
+
+import java.util.Date;
+
+
+/**
+ * <p>Common code for {@link NavigationInstance NavigationInstances}.</p>
+ *
+ * @author Jan Dockx
+ * @author PeopleWare n.v.
+ */
+public class AbstractNavigationInstance {
+
+  /*<section name="Meta Information">*/
+  //------------------------------------------------------------------
+  /** {@value} */
+  public static final String CVS_REVISION = "$Revision$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_DATE = "$Date$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_STATE = "$State$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_TAG = "$Name$"; //$NON-NLS-1$
+  /*</section>*/
+
+
+
+  /*<property name="time">*/
+  //------------------------------------------------------------------
+
+  public final Date getTime() {
+    return $endTime;
+  }
+
+  /**
+   * @post new.getTime().equals(NOW);
+   */
+  protected void resetTime() {
+    $endTime = new Date();
+  }
+
+  /**
+   * @invar $endTime != null;
+   */
+  private Date $endTime = new Date();
+
+  /*</property>*/
+
+}
+
+
+
