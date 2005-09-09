51bc0d5bb96960e57aa2a530ad8a954db01d3936
Jan Dockx <jan_dockx@peopleware.be>
2005-09-09 11:22:30 +0000

    General code for instance handlers that also want to be a TreeNode.
    git-svn-id: file:///repositories/PPWCode/trunk@676 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

---
+++ ppw-jsf/src/java/be/peopleware/jsf_II/myfaces/tree2/AbstractInstanceHandlerTreeNode.java
@@ -0,0 +1,142 @@
+/*<license>
+Copyright 2004-2005, PeopleWare n.v.
+NO RIGHTS ARE GRANTED FOR THE USE OF THIS SOFTWARE, EXCEPT, IN WRITING,
+TO SELECTED PARTIES.
+</license>*/
+
+package be.peopleware.jsf_II.myfaces.tree2;
+
+
+import org.apache.myfaces.custom.tree2.TreeNode;
+
+import be.peopleware.jsf_II.persistence.InstanceHandler;
+
+
+/**
+ * <p>Default implementation of some {@link TreeNode} methods,
+ *   so that this handler can be used as a {@link TreeNode}
+ *   without a wrapper.
+ *   Subclasses should implement {@link #getChildren()},
+ *   and can overwrite {@link #getChildCount()} for
+ *   performance reasons.</p>
+ * <p>The {@link #getType()} of the tree node, to be used as the
+ *   facet name in the jspx page, is the FQCN of the
+ *   {@link #getPersistentBeanType() persistent bean type} we
+ *   are handling.</p>
+ *
+ * @note The {@link TreeNode} interface defines some stupid methods
+ *       that should not appear in an API interface in the first place.
+ *       See <a href="http://issues.apache.org/jira/browse/MYFACES-447">the
+ *       MyFaces JIRA</a>.
+ *
+ * @author Jan Dockx
+ * @author PeopleWare n.v.
+ *
+ * @invar getChildCount() = getChildren().size();
+ * @invar isLeaf() <==> getChildCount() == 0;
+ */
+public abstract class AbstractInstanceHandlerTreeNode extends InstanceHandler implements TreeNode {
+
+  /*<section name="Meta Information">*/
+  //------------------------------------------------------------------
+  /** {@value} */
+  public static final String CVS_REVISION = "$Revision$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_DATE = "$Date$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_STATE = "$State$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_TAG = "$Name$"; //$NON-NLS-1$
+  /*</section>*/
+
+
+  // default constructor only
+
+
+  /*<section name="treeNodeInterFaceImpl">*/
+  //------------------------------------------------------------------
+
+  /**
+   * @return getPersistentBeanType().getName();
+   */
+  public final String getType() {
+    return getPersistentBeanType().getName();
+  }
+
+  /**
+   * Can be overwritten for performance reasons.
+   *
+   * @return getChildren().size();
+   */
+  public int getChildCount() {
+    return 0;
+  }
+
+  /**
+   * @return  getChildCount() == 0;
+   */
+  public boolean isLeaf() {
+    return getChildCount() == 0;
+  }
+
+  /**
+   * NOP; should not be in the interface
+   *
+   * @deprecated
+   */
+  public final void setLeaf(boolean arg0) {
+    // NOP
+  }
+
+  /**
+   * NOP; should not be in the interface
+   *
+   * @deprecated
+   */
+  public final void setType(String arg0) {
+    // NOP
+  }
+
+  /**
+   * should not be in the interface
+   *
+   * @return null;
+   *
+   * @deprecated
+   */
+  public final String getDescription() {
+    return null;
+  }
+
+  /**
+   * NOP; should not be in the interface
+   *
+   * @deprecated
+   */
+  public final void setDescription(String arg0) {
+    // NOP
+  }
+
+  /**
+   * NOP; should not be in the interface
+   *
+   * @deprecated
+   */
+  public final void setIdentifier(String arg0) {
+    // NOP
+  }
+
+  /**
+   * should not be in the interface
+   *
+   * @return null;
+   *
+   * @deprecated
+   */
+  public final String getIdentifier() {
+    return null;
+  }
+
+  /*</section>*/
+
+}
