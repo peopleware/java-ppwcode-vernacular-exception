eff7e8e041aa0d50ca4f52da23a35e3ccc4b0bf5
Nele Smeets <nele_smeets@peopleware.be>
2005-10-06 13:13:24 +0000

    Added missing contracts. Added basic inspectors.
    git-svn-id: file:///repositories/PPWCode/trunk@772 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- ppw-jsf/src/java/be/peopleware/jsf_II/component/PushNavInstanceTag.java
+++ ppw-jsf/src/java/be/peopleware/jsf_II/component/PushNavInstanceTag.java
@@ -42,42 +42,98 @@ public class PushNavInstanceTag extends UIComponentTag {
 
   private static final Log LOG = LogFactory.getLog(PushNavInstanceTag.class);
 
- 
+  /**
+   * Return the component type for the component that is or will be bound to
+   * this tag.
+   *
+   * @return   UIPushNavInstance.class.getName();
+   */
   public String getComponentType() {
     return UIPushNavInstance.class.getName();
   }
 
+  /**
+   * Return the rendererType property that selects the Renderer to be used for
+   * encoding this component, or null to ask the component to render itself
+   * directly.
+   *
+   * @return  null;
+   */
   public String getRendererType() {
     return null;
   }
 
-  /*<property name="handler">*/
+  /*<property name="value">*/
   //------------------------------------------------------------------
 
-  
+  /**
+   * The value of this tag.
+   *
+   * @basic
+   */
+  public String getValue() {
+    return $navigationInstanceExpression;
+  }
+
+  /**
+   * Set the given value.
+   *
+   * @param navigationInstanceExpression
+   * @post  navigationInstanceExpression == null
+   *          ? new.getValue() == null
+   *          : navigationInstanceExpression.equals(new.getValue());
+   */
   public final void setValue(String navigationInstanceExpression) {
     $navigationInstanceExpression = navigationInstanceExpression;
     LOG.debug("navigation instance expression set: " + $navigationInstanceExpression);
   }
-  
+
   private String $navigationInstanceExpression;
-  
+
   /*</property>*/
 
   /*<property name="navigationStack">*/
   //------------------------------------------------------------------
 
-  
+  /**
+   * The navigation stack of this tag.
+   *
+   * @basic
+   */
+  public String getNavigationStack() {
+    return $navigationStackExpression;
+  }
+
+  /**
+   * Set the given navigation stack.
+   *
+   * @param navigationStackExpression
+   * @post  navigationStackExpression == null
+   *          ? new.getNavigationStack() == null
+   *          : navigationStackExpression.equals(new.getNavigationStack());
+   */
   public final void setNavigationStack(String navigationStackExpression) {
     $navigationStackExpression = navigationStackExpression;
     LOG.debug("navigationStack expression set: " + $navigationStackExpression);
   }
-  
+
   private String $navigationStackExpression;
-  
+
   /*</property>*/
 
   /**
+   * Override properties and attributes of the specified component, if the
+   * corresponding properties of this tag handler instance were explicitly set.
+   *
+   * Create a value binding with the expression {@link #getValue()} and add
+   * it to the given component using the name
+   * {@link UIPushNavInstance#NAVIGATION_INSTANCE_VALUE_BINDING_NAME}.
+   *
+   * Create a value binding with the expression {@link #getNavigationStack()} and add
+   * it to the given component using the name
+   * {@link UIPushNavInstance#NAVIGATION_STACK_VALUE_BINDING_NAME}.
+   *
+   * @see    UIComponentTag
    * @throws FatalFacesException
    *         {@link RobustCurrent#creatValueBinding(UIComponent, String, String)};
    */
@@ -85,16 +141,23 @@ public class PushNavInstanceTag extends UIComponentTag {
     super.setProperties(component);
     RobustCurrent.creatValueBinding(component,
                                     UIPushNavInstance.NAVIGATION_INSTANCE_VALUE_BINDING_NAME,
-                                    $navigationInstanceExpression);
+                                    getValue());
     RobustCurrent.creatValueBinding(component,
                                     UIPushNavInstance.NAVIGATION_STACK_VALUE_BINDING_NAME,
-                                    $navigationStackExpression);
+                                    getNavigationStack());
   }
-  
+
+  /**
+  * Release any resources allocated during the execution of this tag handler.
+  *
+  * @see   UIComponentTag
+  * @post  new.getValue() == null;
+  * @post  new.getNavigationStack() == null;
+  */
   public final void release() {
     super.release();
-    $navigationInstanceExpression = null;
-    $navigationStackExpression = null;
+    setValue(null);
+    setNavigationStack(null);
   }
-  
+
 }
