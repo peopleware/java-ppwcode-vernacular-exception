031bf3bc54631ae641bc3112cea9d2db7d9ba921
Nele Smeets <nele_smeets@peopleware.be>
2005-10-06 13:02:20 +0000

    Added missing contracts. Added basic inspector getValue.
    git-svn-id: file:///repositories/PPWCode/trunk@770 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- ppw-jsf/src/java/be/peopleware/jsf_II/component/NumberOfTag.java
+++ ppw-jsf/src/java/be/peopleware/jsf_II/component/NumberOfTag.java
@@ -37,21 +37,63 @@ public class NumberOfTag extends UIComponentTag {
   /*</section>*/
 
 
+  /**
+   * Return the component type for the component that is or will be bound to
+   * this tag.
+   *
+   * @return   UINumberOf.class.getName();
+   */
   public String getComponentType() {
     return UINumberOf.class.getName();
   }
 
+  /**
+   * Return the rendererType property that selects the Renderer to be used for
+   * encoding this component, or null to ask the component to render itself
+   * directly.
+   *
+   * @return  null;
+   */
   public String getRendererType() {
     return null;
   }
 
+  /*<property name="value">*/
+  //------------------------------------------------------------------
+
+  /**
+   * The value of this tag.
+   *
+   * @basic
+   */
+  public String getValue() {
+    return $collectionExpression;
+  }
+
+  /**
+   * Set the given value.
+   *
+   * @param collectionExpression
+   * @post  collectionExpression == null
+   *          ? new.getValue() == null
+   *          : collectionExpression.equals(new.getValue());
+   */
   public void setValue(String collectionExpression) {
     $collectionExpression = collectionExpression;
   }
 
   private String $collectionExpression;
 
+  /*</property>*/
+
   /**
+   * Override properties and attributes of the specified component, if the
+   * corresponding properties of this tag handler instance were explicitly set.
+   *
+   * Create a value binding with the expression {@link #getValue()} and add
+   * it to the given component using the name {@link UINumberOf#COLLECTION_VALUE_BINDING_NAME}.
+   *
+   * @see    UIComponentTag
    * @throws FatalFacesException
    *         {@link RobustCurrent#creatValueBinding(UIComponent, String, String)};
    */
@@ -59,9 +101,15 @@ public class NumberOfTag extends UIComponentTag {
     super.setProperties(component);
     RobustCurrent.creatValueBinding(component,
                                     UINumberOf.COLLECTION_VALUE_BINDING_NAME,
-                                    $collectionExpression);
+                                    getValue());
   }
 
+  /**
+   * Release any resources allocated during the execution of this tag handler.
+   *
+   * @see   UIComponentTag
+   * @post  new.getValue() == null;
+   */
   public final void release() {
     super.release();
     $collectionExpression = null;
