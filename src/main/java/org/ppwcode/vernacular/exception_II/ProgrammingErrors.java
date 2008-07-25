d2348fca50eb87f5317ff77e3ef63efca6f2dfc2
ppw.bot <ppw.bot@peopleware.be>
2008-07-25 15:04:23 +0000

    svn_spider [r1821]
    working root:
      https://ppwcode.googlecode.com/svn/java/vernacular/exception/trunk
    
    fixes:
      pom.xml
        +svn:eol-style:       native
        +svn:keywords:        Date Rev Id URL
        +svn:mime-type:       text/xml
      src/main/changes/changes.xml
        -svn:keywords:        Author Date Id Revision
        +svn:keywords:        Date Rev Id URL
        +svn:mime-type:       text/xml
      src/main/java/org/ppwcode/vernacular/exception_II/ExternalError.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/main/java/org/ppwcode/vernacular/exception_II/InternalException.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/main/java/org/ppwcode/vernacular/exception_II/InternalException.properties
        +svn:eol-style:       native
      src/main/java/org/ppwcode/vernacular/exception_II/InternalException_nl.properties
        +svn:eol-style:       native
      src/main/java/org/ppwcode/vernacular/exception_II/ProgrammingErrors.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/main/java/org/ppwcode/vernacular/exception_II/SecurityException.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/main/java/org/ppwcode/vernacular/exception_II/SecurityException.properties
        +svn:eol-style:       native
      src/main/java/org/ppwcode/vernacular/exception_II/SecurityException_nl.properties
        +svn:eol-style:       native
      src/main/java/org/ppwcode/vernacular/exception_II/SemanticException.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/main/java/org/ppwcode/vernacular/exception_II/SemanticException.properties
        +svn:eol-style:       native
      src/main/java/org/ppwcode/vernacular/exception_II/SemanticException_nl.properties
        +svn:eol-style:       native
      src/main/java/org/ppwcode/vernacular/exception_II/doc-files/exceptionHierarchy.png
        -svn:mime-type:       application/octet-stream
        +svn:mime-type:       image/png
      src/main/java/org/ppwcode/vernacular/exception_II/overview.html
        +svn:mime-type:       text/html
      src/test/java/org/ppwcode/vernacular/exception_II/AssertTest.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/vernacular/exception_II/TestExternalError.java
        -svn:keywords:        Author Date Id Revision
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/vernacular/exception_II/TestInternalException.java
        -svn:keywords:        Author Date Id Revision
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/vernacular/exception_II/TestProgrammingError.java
        -svn:keywords:        Author Date Id Revision
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/vernacular/exception_II/TestSecurityException.java
        -svn:keywords:        Author Date Id Revision
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/vernacular/exception_II/TestSemanticException.java
        -svn:keywords:        Author Date Id Revision
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      statcvs.xml
        -svn:keywords:        Author Date Id Revision
        +svn:keywords:        Date Rev Id URL
        +svn:mime-type:       text/xml
    
    
    git-svn-id: file:///repositories/PPWCode/java/vernacular/exception/trunk@1827 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/exception_II/ProgrammingErrors.java
+++ src/main/java/org/ppwcode/vernacular/exception_II/ProgrammingErrors.java
@@ -1,5 +1,5 @@
 /*<license>
-Copyright 2004 - $Date: 2008-07-02 11:36:19 +0200 (Wed, 02 Jul 2008) $ by PeopleWare n.v..
+Copyright 2004 - $Date$ by PeopleWare n.v..
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -85,10 +85,10 @@ import org.toryt.annotations_I.Throw;
  * @author    Jan Dockx
  * @author    PeopleWare n.v.
  */
-@Copyright("2004 - $Date: 2008-07-02 11:36:19 +0200 (Wed, 02 Jul 2008) $, PeopleWare n.v.")
+@Copyright("2004 - $Date$, PeopleWare n.v.")
 @License(APACHE_V2)
-@SvnInfo(revision = "$Revision: 1504 $",
-         date     = "$Date: 2008-07-02 11:36:19 +0200 (Wed, 02 Jul 2008) $")
+@SvnInfo(revision = "$Revision$",
+         date     = "$Date$")
 @Invars({
   @Expression("message != null"),
   @Expression("message != EMPTY")
