1caa3f503937f956c9417d962781c647e8d11392
ppw.bot <ppw.bot@peopleware.be>
2008-10-03 14:39:11 +0000

    svn_spider [r1824]
    working root:
      https://ppwcode.googlecode.com/svn/java/vernacular/exception/trunk
    
    fixes:
      src/main/java/org/ppwcode/vernacular/exception_II/ExceptionHelpers.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
    
    
    git-svn-id: file:///repositories/PPWCode/java/vernacular/exception/trunk@2906 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/exception_II/ExceptionHelpers.java
+++ src/main/java/org/ppwcode/vernacular/exception_II/ExceptionHelpers.java
@@ -1,5 +1,5 @@
 /*<license>
-Copyright 2004 - $Date: 2008-08-05 13:44:45 +0200 (Tue, 05 Aug 2008) $ by PeopleWare n.v..
+Copyright 2004 - $Date$ by PeopleWare n.v..
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -36,10 +36,10 @@ import org.toryt.annotations_I.MethodContract;
  * @author      Jan Dockx
  * @author      PeopleWare n.v.
  */
-@Copyright("2007 - $Date: 2008-08-05 13:44:45 +0200 (Tue, 05 Aug 2008) $, PeopleWare n.v.")
+@Copyright("2007 - $Date$, PeopleWare n.v.")
 @License(APACHE_V2)
-@SvnInfo(revision = "$Revision: 2269 $",
-         date     = "$Date: 2008-08-05 13:44:45 +0200 (Tue, 05 Aug 2008) $")
+@SvnInfo(revision = "$Revision$",
+         date     = "$Date$")
 public class ExceptionHelpers {
 
   /**
